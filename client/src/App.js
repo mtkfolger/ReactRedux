8ef013a6dac86db3164d7a9a66b5e2d1eb7169f3
Michael Folger <folger@zoller-usa.com>
2021-11-04 23:44:26 -0500

    fixed typos that weren't allowing site to compile

--- client/src/App.js
+++ client/src/App.js
@@ -44,7 +44,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <Provider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './app/store'
-import {Provider} from 'react-redux'
 
 ReactDOM.render(
-  <Provider>
-      <App />
-    </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
